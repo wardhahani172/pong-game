0818e8080c7221cb2b5926ddd65666f12cc0490c
kafitawardhahani <wardha.ancip72@gmail.com>
2020-03-23 17:31:01 +0700

    commitan pertama

---
+++ Assets/BallCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.collider.name == "TembokKanan" || other.collider.name == "TembokKiri"){
+            GetComponent<Transform>().position = new Vector2(0,0);
+            //GetComponent<Rigidbody2D>().velocity = new Vector2 (-1,-1) * 30;
+        }
+    }
+}
---
+++ Assets/ball.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public int speed = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+       GetComponent<Rigidbody2D>().velocity = new Vector2(1,1) * speed; 
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+}
